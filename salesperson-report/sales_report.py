a378e0e42990fafe834e01285eb87fc4b0d223cd
Henry Hsu <h_hsu817@yahoo.com>
2022-04-05 14:16:36 -0700

    Implements improvements

--- salesperson-report/sales_report.py
+++ salesperson-report/sales_report.py
@@ -1,30 +1,23 @@
 """Generate sales report showing total melons each salesperson sold."""
 
-# Create 2 separate lists to track salespeople & corresponding amount 
-# of melons sold for each salesperson
-salespeople = []
-melons_sold = []
+# improvement: consolidate the 2 separate lists as 1 dictionary
+sales_info = {}
 
 # Open 'sales-report.txt' for parsing
 f = open('sales-report.txt')
 
 # iterate over each line of the input file for data processing
+# improvement: update code to perform read/write operations to new dictionary
 for line in f:
     line = line.rstrip()            # strip out white space at end of each line
     entries = line.split('|')       # splits each line using '|' as delimiter, store result as a list
     salesperson = entries[0]        # First element of the list contains the salesperson's name, save that as 'salesperson' variable
-    melons = int(entries[2])        # convert the number of melons sold into Integer data type and save it as 'melons' variable
+    melons_sold = int(entries[2])   # convert the number of melons sold into Integer data type and save it as 'melons_sold' variable
 
-    # Find the index of a salesperson if they exist in the 'salespeople' list, and use that index to update amount 
-    # of melons sold in 'melons_sold' list
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
-        melons_sold[position] += melons
-        
-    else: # Otherwise, add their name to 'salespeople' list and amount of melons sold to 'melons_sold' list
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+    # Add salesperson and melons sold info to 'sales_info' dictionary if they don't have an existing entry, 
+    # otherwise update their amount of melons sold
+    sales_info[salesperson] = melons_sold if salesperson not in sales_info else sales_info[salesperson] + melons_sold
 
-# Iterate over 'salespeople' list, using current index to print out the salesperson's name and corresponding amount of melons sold
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+# Iterate over 'sales_info' dictionary, and print out each salesperson and their amount of melons sold
+for key, value in sales_info.items():
+    print(f'{key} sold {value} melons')
