38886fa4ab42f9368b7687b61e7445629f5b6fa2
Henry Hsu <h_hsu817@yahoo.com>
2022-04-18 10:13:51 -0700

    Implements javascript equivalent of Python function

---
+++ javascript/melonCounting.js
@@ -0,0 +1,25 @@
+
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+'Watermelon', 'Santa Claus', 'Casaba']
+
+
+function countMelons(melon_list) {
+    // Take in a list and return a dictionary of # of melons by melon type
+
+    const answer = {};
+
+    melon_list.forEach(melon => answer[melon] = (answer[melon] || 0) +1);
+
+    return(answer);
+}
+
+console.log(countMelons(melons_to_add));
